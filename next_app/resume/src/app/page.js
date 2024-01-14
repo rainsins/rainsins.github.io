f89847f0e15535190e02933206e28646ff4f29c4
rainsins <1820278582@qq.com>
2024-01-14 14:34:15 +0800

    update

--- next_app/resume/src/app/page.js
+++ next_app/resume/src/app/page.js
@@ -1,31 +1,118 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+const data = {
+  name:"刘华强",
+  dev:"劈生瓜蛋子",
+  contact:{
+    mail:"rainsin@qq.com",
+    phone:"18193847354",
+    loacl:"甘肃 天水",
+    wechat:"18193847354",
+    time:"2000.03.27"
+  },
+  skills:{
+      lang: [
+        {
+          name: "Latex",
+          shoulian:5,
+        },
+        {
+          name: "C++",
+          shoulian:5,
+        },
+        {
+          name: "JavaScript",
+          shoulian:5,
+        },
+        {
+          name: "HTML和CSS",
+          shoulian:5,
+        },
+        {
+          name: "Python",
+          shoulian:3,
+        },
+        {
+          name: "Rust",
+          shoulian:2,
+        }
+      ],
+      ku:[
+        {
+          name: "React",
+          shoulian:4,
+        },
+        {
+          name: "Markdown",
+          shoulian:4,
+        },
+        {
+          name: "Linux",
+          shoulian:4,
+        },
+        {
+          name: "Git",
+          shoulian:4,
+        },
+      ],
+      tho:[
+        {
+          name:"",
+          shoulian:""
+        }
+      ]
+  },
+
+  project:[
+    {
+      name:"TCP/IP的C++实现",
+      author:"尉旭胜",
+      time:"2023.12",
+      detail: [
+        "实现了TCP/IP的常用协议",
+        ""
+      ]
+    },
+  ],
+  edu:[
+    {
+      name:"",
+      prof:"",
+      start_time:"",
+      end_time:"",
+      detail:[
+        "",
+        ""
+      ]
+    }
+  ],
+  p:""
+}
+
 export default function Home() {
-  const img_style = {
-    
-  }
   return (
     <main className={styles.main}>
       <div className={styles.resume_box}>
-        <div className={styles.resume_left_box}>
-          <div className='resume_img_info'>
-            <Image
-              src="https://rainsin-1305486451.file.myqcloud.com/rainsin-blog/resume/FFF3921FD2192061589961B9A1F_70AA3B53_AEAC.jpg"
-              alt='长安大学'
-              width={64}
-              height={64}
-            />
-          </div>
-          <div className='resume_name'>
-            尉旭胜
-          </div>
-          <div className='resume_dev'>
-            Full Stack Developer
-          </div>
+        <div className={styles.resume_info_box}>
+          <div className={styles.resme_name}></div>
+          <div className={styles.resme_dev}></div>
+          <div className={styles.resme_contact}></div>
         </div>
-        <div className={styles.resume_right_box}>
-          
+        <div className={styles.resume_info_box}>
+          <div className={styles.resme_name}></div>
+          <div className={styles.resme_dev}></div>
+          <div className={styles.resme_contact}></div>
+        </div>
+        <div className={styles.resume_info_box}>
+          <div className={styles.resme_name}></div>
+          <div className={styles.resme_dev}></div>
+          <div className={styles.resme_contact}></div>
+        </div>
+        <div className={styles.resume_info_box}>
+          <div className={styles.resme_name}></div>
+          <div className={styles.resme_dev}></div>
+          <div className={styles.resme_contact}></div>
         </div>
       </div>
     </main>
