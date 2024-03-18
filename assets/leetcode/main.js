6cfffd64742b53f79d27a2a6d8858da799c219bf
rainsins <1820278582@qq.com>
2024-03-18 19:03:14 +0800

    update leetcode

---
+++ assets/leetcode/main.js
@@ -0,0 +1,5 @@
+import { CopyBlock } from 'https://cdn.jsdelivr.net/npm/react-code-blocks@0.1.6/+esm'
+
+
+const leet_boxs = ReactDOM.createRoot(document.getElementById('leetcode'));
+leet_boxs.render(<Leetcode />);ß
