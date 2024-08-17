cdd75d89602fad14894c12e0e5eacff7b569b298
rainsins <1820278582@qq.com>
2024-08-17 22:43:49 +0800

    update

--- assets/links/bg.js
+++ assets/links/bg.js
@@ -1,23 +1,21 @@
 
 function LinksBg() {
-    const arr = new Array(13);
+    const arr = new Array(13).fill('0');
 
     const item = arr.map((e, i) => {
         const tr = {
-            transform: `rotate(${Math.random() > 0.5 ? '-' : ''}${Math.floor(Math.random()* 6)}deg)`
+            transform: `rotate(${Math.random() > 0.5 ? '-' : ''}${Math.floor(Math.random() * 6)}deg)`
         }
         return (
-            <>
-                <div className="card" style={{tr}}><img src={`https://pan.rainsin.cn:2000/d/blog/img/post/%E5%8D%81%E4%BA%8C%E6%A2%A6_%E4%BA%AE%E7%82%B9%E6%BC%AB%E7%94%BB-%E7%AB%99%E9%85%B7ZCOOL/100${i + 1}.jpg`} />
-                    <h2>{ i + 1}</h2>
-                </div>
-            </>
+            <div className="card" style={{ tr }}><img src={`https://pan.rainsin.cn:2000/d/blog/img/post/%E5%8D%81%E4%BA%8C%E6%A2%A6_%E4%BA%AE%E7%82%B9%E6%BC%AB%E7%94%BB-%E7%AB%99%E9%85%B7ZCOOL/100${i + 1}.jpg`} />
+                <h2>{i + 1}</h2>
+            </div>
         )
     })
 
     return (
         <>
-            { item}
+            {item}
         </>
     )
 }
