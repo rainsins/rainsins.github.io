816a8c2514b196142b3efb768bf97f94a33a6355
rainsins <1820278582@qq.com>
2024-09-09 11:47:31 +0800

    update heti

--- assets/js/heti.js
+++ assets/js/heti.js
@@ -1,13 +1,73 @@
+//   定义栈的类
+class bracketMatchStack {
+    constructor() {
+        this.stack = [];
+    }
+    push(item) {
+        return this.stack.push(item);
+    }
+    pop() {
+        return this.stack.pop();
+    }
+    // 查询栈顶的元素
+    peek() {
+        return this.stack[this.getSize() - 1];
+    }
+    //返回栈的长度
+    getSize() {
+        return this.stack.length;
+    }
+    // 栈的非空判断
+    isEmpty() {
+        return this.getSize() === 0;
+    }
+}
+
+function testBracketIsValid(str) {
+    // 以左右括号来建立一个对象，key为左括号，value为右括号
+    var Map = {
+        "{": "}",
+        "(": ")",
+        "[": "]",
+    };
+    //实例化一个栈
+    const myStack = new bracketMatchStack();
+    //遍历str字符串
+    for (let v of str) {
+        if (Map[v]) {
+            myStack.push(v); //是左括号，入栈
+        } else if (Object.values(Map).includes(v)) {
+            // 右括号  将当前的元素和栈顶的第一个元素进行匹配
+            let last = myStack.pop();
+            if (v !== Map[last]) return false;
+        } else {
+            //这里排除的是空字符的情况，如果不是左右括号而是其他的空字符串或者非法字符的话，将终止本次循环，执行下一次循环
+            continue;
+        }
+    }
+    //遍历完成之后要保证栈内要为空
+    return myStack.getSize() === 0;
+}
+
+
+//转换为HTML
 function creactObject(text) {
+
     if (text.length == 1) {
         return;
     }
 
+    //中间变量
     let Ht_type = "";
     let Ht_content = "";
+
+    //获取的元素类型
     let ele_type = "";
+    //获取的元素属性
     let ele_first = "";
 
+    //判断text
+    //段首不是指令开头和结束
     if (text.indexOf("@H") !== -1 && text.indexOf("@H") === 0) {
         const Ht_si = text.indexOf("@H");
         const Ht_ei = text.indexOf("{");
@@ -21,8 +81,10 @@ function creactObject(text) {
         let classes = "";
         let c = "";
 
+        //入栈
         stack_main.push(ele_type[0]);
 
+        //可以增删指令
         switch (ele_type[0]) {
             case "div":
                 classes = ele_first[0].replace(/\s*/g, "").split(",");
@@ -107,7 +169,7 @@ function creactObject(text) {
             case "q":
                 result += `<q>`;
                 break;
-            case "addr":
+            case "abbr":
                 c = `title="${ele_first[0]}"`;
                 result += `<abbr ${c}>`;
                 break;
@@ -117,11 +179,47 @@ function creactObject(text) {
             case "tem":
                 result += `<span class="heti-em">`;
                 break;
+            case "h1":
+                result += `<h1>`;
+                break;
+            case "h3":
+                result += `<h3>`;
+                break;
+            case "h4":
+                result += `<h4>`;
+                break;
+            case "h5":
+                result += `<h5>`;
+                break;
+            case "ul":
+                result += `<ul>`;
+                break;
+            case "li":
+                result += `<li>`;
+                break;
+            case "i":
+                result += `<i>`;
+                break;
+            case "sup":
+                result += `<sup>`;
+                break;
+            case "sub":
+                result += `<sub>`;
+                break;
+            case "code":
+                result += `<code>`;
+                break;
+            case "dfn":
+                result += `<dfn>`;
+                break;
+            case "s":
+                result += `<s>`;
+                break;
             default:
                 console.log(`Sorry, we are out of.`);
                 break;
         }
-
+        //处理文字开头的情况
         if (Ht_content[0] !== "@" && Ht_content[0] !== "}") {
             const Ht_si = 0;
             const isbehind = Ht_content.indexOf("@H") < Ht_content.indexOf("}");
@@ -155,6 +253,8 @@ function creactObject(text) {
         text = text.substring(Ht_ei, text.length);
         creactObject(text);
     } else if (text.indexOf("}") == 0) {
+        //遇到 } 就会有标签闭合，即栈顶元素闭合。
+
         text = text.substring(1, text.length);
         switch (stack_main.pop()) {
             case "div":
@@ -181,7 +281,7 @@ function creactObject(text) {
             case "q":
                 result += `</q>`;
                 break;
-            case "addr":
+            case "abbr":
                 result += `</abbr>`;
                 break;
             case "mr":
@@ -193,6 +293,42 @@ function creactObject(text) {
             case "text":
                 result += ``;
                 break;
+            case "h1":
+                result += `</h1>`;
+                break;
+            case "h3":
+                result += `</h3>`;
+                break;
+            case "h4":
+                result += `</h4>`;
+                break;
+            case "h5":
+                result += `</h5>`;
+                break;
+            case "ul":
+                result += `</ul>`;
+                break;
+            case "li":
+                result += `</li>`;
+                break;
+            case "i":
+                result += `</i>`;
+                break;
+            case "sup":
+                result += `</sup>`;
+                break;
+            case "sub":
+                result += `</sub>`;
+                break;
+            case "code":
+                result += `</code>`;
+                break;
+            case "dfn":
+                result += `<dfn>`;
+                break;
+            case "s":
+                result += `<s>`;
+                break;
             default:
                 console.log(`Sorry, we are out of.`);
                 break;
@@ -202,6 +338,11 @@ function creactObject(text) {
 }
 
 function heti(str) {
+    if (!testBracketIsValid(str)) {
+        console.log("指令好像不对");
+        return $("<div>指令好像不对</div>");
+    }
+
     window.stack_main = [];
     window.result = "";
 
@@ -211,12 +352,15 @@ function heti(str) {
     return $(result);
 }
 
+//遍历要作用的元素
 $(".heti-box").each(function () {
     const _self = $(this);
     const _html = _self.html();
     _self.html("");
     _self.append(heti(_html));
 });
+
+//获取拼音
 $("py").each(function () {
     const _self = $(this);
     const pp = _self.data("pinyin") ? _self.data("pinyin").split(",") : false;
