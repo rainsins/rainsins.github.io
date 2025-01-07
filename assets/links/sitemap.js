dce2ba9965cfb30babda06aa7dd553cf0f63ee4c
rainsins <1820278582@qq.com>
2025-01-07 18:55:58 +0800

    update rss:更新

--- assets/links/sitemap.js
+++ assets/links/sitemap.js
@@ -1,33 +1,16 @@
 const fs = require("fs");
-const links_data = require("./linksdata.js");
 const os = require('os');
 const xml2js = require('xml2js');
 
-fs.readFile("../../sitemap.xml", "utf-8", (err, data) => {
+fs.readFile("../../_site/sitemap.xml", "utf-8", (err, data) => {
     if (err) throw err;
     // console.log(data);
     xml2js.parseString(data, (err, result) => {
         if (err) throw err;
 
         links_data.forEach((e, i) => {
-            if (i != 0) {
-                const post_data = {
-                    loc:`${e.url}`,
-                    priority: "1.0"
-                };
-    
-                result.urlset.url.push(post_data);
-                fs.appendFileSync("../../sitemap.txt", `${e.url}${os.EOL}`,'utf8');
-            }
-        });
-
-        const builder = new xml2js.Builder();
-        const xml = builder.buildObject(result);
-
-        fs.writeFile('../../sitemap.xml', xml, (err) => {
-            if (err) throw err;
-
-            console.log(`Updated XML is written to a new file.`);
+            
+            fs.appendFileSync("../../_site/sitemap.txt", `${e.url}${os.EOL}`,'utf8');
         });
     });
 });
