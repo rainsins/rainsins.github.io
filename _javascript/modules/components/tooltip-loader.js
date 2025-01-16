a4e8669e98b5d42b916d72303dc8177ffdadd5f7
rainsins <1820278582@qq.com>
2025-01-16 14:56:42 +0800

    update tooltip:修改提示插件

--- _javascript/modules/components/tooltip-loader.js
+++ _javascript/modules/components/tooltip-loader.js
@@ -2,21 +2,4 @@
  * Initial Bootstrap Tooltip.
  */
 export function loadTooptip() {
-  const tooltipTriggerList = document.querySelectorAll(
-    '[data-bs-toggle="tooltip"]'
-  );
-
-  [...tooltipTriggerList].map(
-    (tooltipTriggerEl) => {
-      return $(tooltipTriggerEl).data("bs-title") ? tippy(tooltipTriggerEl, {
-        content: $(tooltipTriggerEl).data("bs-title"),
-        placement: 'top',
-        theme: 'translucent',
-      }) : tippy(tooltipTriggerEl, {
-        content: $(tooltipTriggerEl).data("bs-original-title"),
-        placement: 'top',
-        theme: 'translucent',
-      });
-    }
-  );
 }
