45d9d762f21bbc3b1a3892d53409009038b79ce1
rainsins <1820278582@qq.com>
2025-06-03 15:18:34 +0800

    update image

--- assets/post/video/video.js
+++ assets/post/video/video.js
@@ -13,6 +13,21 @@ function playMpd(video, url, art) {
     }
 }
 
+function playM3u8(video, url, art) {
+    if (Hls.isSupported()) {
+        if (art.hls) art.hls.destroy();
+        const hls = new Hls();
+        hls.loadSource(url);
+        hls.attachMedia(video);
+        art.hls = hls;
+        art.on('destroy', () => hls.destroy());
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = url;
+    } else {
+        art.notice.show = 'Unsupported playback format: m3u8';
+    }
+}
+
 async function send_message() {
     const password = $("#email-field").val();
 
@@ -26,7 +41,7 @@ async function send_message() {
     }).toString();
 
     if (!isLoad) {
-        fetch(`https://myexapi.hk.cpolar.io:2000/blog/envideo/${query}`)
+        fetch(`https://myapi.rainsin.cn/blog/envideo/${query}`)
             .then((response) => {
                 isLoad = true;
                 if (response.status == "404") {
@@ -41,7 +56,7 @@ async function send_message() {
                     art.destroy();
                     art = new Artplayer({
                         container: '#video-box',
-                        url: 'https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/up/Sone-544/main.mpd',
+                        url: 'https://pan.rainsin.cn/d/aliup/win.DESKTOP-PLI9GE8/up/Sone-544/main.mpd',
                         type: 'mpd',
                         theme: "#2c9678",
                         title: 'Sone-544',
@@ -82,16 +97,74 @@ async function send_message() {
 }
 
 
+const video = [
+    "西游记（1986）",
+    [
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5222486782687309806_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5221923836211844606_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5189835687545858667_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5222205307782734748_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5197998463397799646_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5248382480897511687_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5255982306920155520_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5245004783362852874_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5188991261818113744_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5245286258564595605_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5242190033819283686_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5195465185500328899_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5233182832465375578_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5257952633664504505_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5239093806671579134_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5194339285053330625_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5212353685308148894_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5196028138193985933_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5201657637211300344_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5192368961005814924_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5234590208336898334_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5231212506629016061_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5201094684845468508_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5245567730520243844_hlsb.m3u8",
+        "https://txmov2.a.kwimgs.com/bs3/video-hls/5215168436118306105_hlsb.m3u8"
+    ],
+    [
+        "猴王初问世",
+        "官封弼马温",
+        "大圣闹天宫",
+        "困囚五行山",
+        "猴王保唐僧",
+        "祸起观音院",
+        "计收猪八戒",
+        "坎途逢三难",
+        "偷吃人参果",
+        "三打白骨精",
+        "智激美猴王",
+        "夺宝莲花洞",
+        "除妖乌鸡国",
+        "大战红孩儿",
+        "斗法降三怪",
+        "趣经女儿国",
+        "三调芭蕉扇",
+        "扫塔辨奇冤",
+        "误入小雷音",
+        "孙猴巧行医",
+        "错坠盘丝洞",
+        "四探无底洞",
+        "传艺玉华洲",
+        "天竺收玉兔",
+        "波生极乐天"
+    ]
+];
+
 
 window.load_event = {
     ...window.load_event,
     artsss: () => {
         art = new Artplayer({
             container: '#video-box',
-            url: 'https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E01.2007.WEB-DL.1080p.HE/main.mpd',
-            type: 'mpd',
+            url: 'https://txmov2.a.kwimgs.com/bs3/video-hls/5222486782687309806_hlsb.m3u8',
+            type: 'm3u8',
             theme: "#2c9678",
-            title: '第1集',
+            title: '猴王初问世',
             flip: true,
             playbackRate: true,
             screenshot: true,
@@ -105,7 +178,7 @@ window.load_event = {
             setting: true,
             autoOrientation: true,
             customType: {
-                mpd: playMpd
+                m3u8: playM3u8
             },
             plugins: [artplayerPlaylist({
                 rebuildPlayer: false, // 换P时重建播放器，默认false
@@ -114,192 +187,16 @@ window.load_event = {
                 },
                 autoNext: true, // 自动播放下一P, 默认false
                 showText: false, // 在控制栏显示文本，否则显示图标，默认为false
-                playlist: [
-                    {
-                        title: "大明王朝1566 第01集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E01.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第02集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E02.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第03集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E03.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第04集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E04.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第05集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E05.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第06集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E06.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第07集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E07.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第08集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E08.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第09集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E09.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第10集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E10.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第11集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E11.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第12集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E12.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第13集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E13.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第14集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E14.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第15集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E15.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第16集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E16.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第17集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E17.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第18集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E18.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第19集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E19.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第20集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E20.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第21集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E21.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第22集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E22.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第23集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E23.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第24集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E24.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第25集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E25.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第26集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E26.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第27集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E27.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第28集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E28.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第29集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E29.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第30集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E30.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第31集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E31.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第32集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E32.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第33集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E33.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第34集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E34.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第35集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E35.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第36集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E36.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第37集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E37.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第38集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E38.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第39集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E39.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第40集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E40.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第41集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E41.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第42集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E42.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第43集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E43.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第44集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E44.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第45集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E45.2007.WEB-DL.1080p.HE/main.mpd"
-                    },
-                    {
-                        title: "大明王朝1566 第46集",
-                        url: "https://pans.rainsin.cn:2000/d/aliup/win.DESKTOP-PLI9GE8/剧集/大明/[47BT]大明王朝1566.Ming.Dynasty.in.1566.E46.2007.WEB-DL.1080p.HE/main.mpd"
-                    }
-                ]
+                playlist: () => {
+                    let arr = [];
+                    video[1].forEach((el, index) => {
+                        arr.push({
+                            title: video[2][index],
+                            url: el
+                        })
+                    })
+                    return arr;
+                },
             })]
         }, function onReady(art) { this.pause() });
     }
